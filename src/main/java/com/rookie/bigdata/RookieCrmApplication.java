87348d0511d4a633e07a13768d50adcc102b9785
liuxili01 <liuxili@bestseller.com.cn>
2023-05-25 16:25:07 +0800

    init

---
+++ src/main/java/com/rookie/bigdata/RookieCrmApplication.java
@@ -0,0 +1,13 @@
+package com.rookie.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookieCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RookieCrmApplication.class, args);
+    }
+
+}
