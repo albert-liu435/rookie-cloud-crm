e5c5d3d2790ba82f815373400242488bdca85346
rookie <liuxili@bestseller.com.cn>
2024-09-26 18:03:32 +0800

    框架搭建

---
+++ rookie-crm/rookie-scrm-api/src/main/java/com/rookie/bigdata/controller/SPITaoBaoController.java
@@ -0,0 +1,21 @@
+package com.rookie.bigdata.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Class SPITaoBaoController
+ * @Description
+ * @Author rookie
+ * @Date 2024/9/26 15:44
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("spi")
+@Slf4j
+public class SPITaoBaoController {
+
+
+
+}
